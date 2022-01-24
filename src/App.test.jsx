cb5460b5b0b74479c10a17f9250e212f1e3d9403
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-01-24 15:41:37 -0800

    first commit of the day, it has been rough, nothing is working yet

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,17 @@
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('Should render the header', () => {
+test('Should render the header', async () => {
+  render(<App />)
 
+  const logo = screen.getByAltText(/alchemy/i)
+  const banner = screen.getByRole('banner')
+  const userName = await screen.getByText(/vonta/i)
+
+  expect(logo).toBeInTheDocument()
+  expect(userName).toBeInTheDocument()
+  expect(banner).toHaveStyle({ background: 'var(--grey' })
 })
+
+// expect(banner).toBeInTheDocument()
+// const banner = await screen.getByRole('banner', { paragraph: { name: /vonta/i } })

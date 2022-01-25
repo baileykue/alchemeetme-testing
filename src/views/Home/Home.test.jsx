25ef029a97b056bd00c384a2d49c41301f0ace27
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-01-24 16:54:16 -0800

    second test passing, whew! made the same mistake switching up get and find, but was able to figure out my mistake the second time around

--- src/views/Home/Home.test.jsx
+++ src/views/Home/Home.test.jsx
@@ -1,3 +1,6 @@
+import { screen, render } from '@testing-library/react'
+import Home from './Home'
+
 const user = {
   id: 1,
   created_at: '2021-12-13T00:17:29+00:00',
@@ -9,4 +12,26 @@ const user = {
   color: 'crimson',
 }
 
-test.skip('Should render the user profile', () => {})
+test('Should render the user profile', async () => {
+  render(<Home user={user} />)
+
+  const { name, color, motto, likes } = user
+
+  const profileBackground = await screen.findByAltText('header')
+  const profilePhoto = screen.getByAltText('avatar')
+
+  const userMotto = screen.getByText(motto)
+  const favColor = screen.getByText(color)
+
+  const userName = screen.getByRole('heading', { name })
+  const userInterests = screen.getByRole('heading', { name: /interests/i })
+  const userList = screen.getByRole('list')
+
+  expect(profileBackground).toBeInTheDocument()
+  expect(profilePhoto).toBeInTheDocument()
+  expect(userMotto).toBeInTheDocument()
+  expect(favColor).toBeInTheDocument()
+  expect(userName).toBeInTheDocument()
+  expect(userInterests).toBeInTheDocument()
+  expect(userList.children.length).toEqual(likes.length)
+})

0b0027876b313f9317256791c9dd6e0c34e615a1
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-01-24 16:28:18 -0800

    first test passing, checking for user name and logo

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,13 +4,13 @@ import App from './App'
 test('Should render the header', async () => {
   render(<App />)
 
+  const userName = await screen.findByText(/vonta/i)
   const logo = screen.getByAltText(/alchemy/i)
   const banner = screen.getByRole('banner')
-  const userName = await screen.getByText(/vonta/i)
 
   expect(logo).toBeInTheDocument()
   expect(userName).toBeInTheDocument()
-  expect(banner).toHaveStyle({ background: 'var(--grey' })
+  expect(banner).toHaveStyle({ background: '--grey' })
 })
 
 // expect(banner).toBeInTheDocument()
